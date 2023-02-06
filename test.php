f4e32f262ad8d13a9c18b4c6c90d8ab735175075
91ahmed <ahmedh12491@gmail.com>
2023-02-06 07:47:14 +0200

    Delete some folders

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require ('vendor/autoload.php');
-
-	$csv = new Csv\Csvtosql\TransformCsv();
-	$csv->file('files/sales.csv')
-	    ->table('salse')
-	    ->transform()
-	    ->exportSQL('exported/sales.sql');
-?>
